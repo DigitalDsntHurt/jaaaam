837ebd37bc341f1b70a41ef84490f15a31c863af
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-01-08 11:11:11 -0700

    More content, more styles

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ function App() {
   return (
     <div className="App">
       <div className="landing-sect light-theme">
-        <p>Welcome</p>
+        <h1>Welcome</h1>
+        <text>Teachings and practices focused on learning to enjoy our bodies. Fun, play, love, creativity, awareness and movement not repetitive, grinding fitness.</text>
       </div>
       <div className="landing-sect dark-theme">
-        <p>Vision</p>
+        <h1>Vision</h1>
+        <text>Your body is a miracle. Learn to enjoy it.</text>
       </div>
       <div className="landing-sect light-theme">
-        <p>Let's move together</p>
+        <h1>Let's move together</h1>
+        <table>
+          <tr>
+            <td className="sexy-cell"><text>Private Sessions</text></td>
+            <td className="sexy-cell"><text>Community Classes</text></td>
+          </tr>
+        </table>
+
       </div>
       <div className="landing-sect dark-theme">
-        <p>Students</p>
+        <h1>Students</h1>
+        <text>"Big quote about working with Nick privately here" -- Student</text>
+        <text>"Big quote about working with Nick in community classes here" -- Student</text>
       </div>
     </div>
   );
