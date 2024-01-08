cbe7a39a7e0d03130f04adf13d4373469dc2245e
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-01-08 10:47:44 -0700

    Major sections + section styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
           Text goes here. Not too big. Bigger than para, though.
         </p>
@@ -18,7 +18,19 @@ function App() {
       </header>
       <body className="body-container">
         <p>some text</p>
-      </body>
+      </body> */}
+      <div className="landing-sect-light">
+        <p>Welcome</p>
+      </div>
+      <div className="landing-sect-dark">
+        <p>Vision</p>
+      </div>
+      <div className="landing-sect-light">
+        <p>Let's move together</p>
+      </div>
+      <div className="landing-sect-dark">
+        <p>Students</p>
+      </div>
     </div>
   );
 }
