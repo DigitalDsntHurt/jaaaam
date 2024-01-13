523057636888519059592d4cd9094019f2cd0ca1
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-01-12 23:41:48 -0700

    Increases title font size, removes subhead

--- src/ImageBanner.js
+++ src/ImageBanner.js
@@ -4,11 +4,12 @@ function ImageBanner({ theme }) {
     return (
         <div className={`landing-sect ${theme}-theme`} >
             <h1 className="banner-image-title">Move</h1>
-            <text className="banner-image-text">Your body, your mind, your life: free, fun, easy.</text>
-            <img src="./images/diagonal_stretch.png" className="banner-image" alt="alt text here" />
-            {/* <img src="./images/half_moon.png" className="banner-image" alt="alt text here" /> */}
-            {/* <img src="./images/sisyphus_squat.png" className="banner-image" alt="alt text here" /> */}
-            {/* <img src="./images/sisyphus_split_leg.png" className="banner-image" alt="alt text here" /> */}
+            {/* <text className="banner-image-text">Your body, your mind, your life: free, fun, easy.</text> */}
+
+            {/* <img id="banner1" src="./images/diagonal_stretch.png" className="banner-image" alt="alt text here" /> */}
+            {/* <img id="banner2" src="./images/half_moon.png" className="banner-image" alt="alt text here" /> */}
+            <img id="banner3" src="./images/sisyphus_squat.png" className="banner-image" alt="alt text here" />
+            {/* <img id="banner4" src="./images/sisyphus_split_leg.png" className="banner-image" alt="alt text here" /> */}
         </div >
     );
 }
