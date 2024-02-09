5af7d311073789d8fb7bd5f5cd42cddab231f2f5
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-09 09:19:47 -0800

    Draws structure for student  testimonials component

--- src/Students.js
+++ src/Students.js
@@ -1,9 +1,31 @@
+import './Students.css';
+
 const Students = ({ theme }) => {
     return (
         <div className={`landing-sect ${theme}-theme`} >
             <h1>Students</h1>
-            <text>"Big quote about working with Nick privately here" -- Student</text>
-            <text>"Big quote about working with Nick in community classes here" -- Student</text>
+            <div className="students-div-wrapper">
+                <div className="students-div-row-1">
+                    <div class='student-testimonial-box'>
+                        <text>"Big quote about working with Nick privately here" -- Student</text>
+                    </div>
+                    <div class='student-testimonial-box'>
+                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                    </div>
+                </div>
+                <div className="students-div-row-2">
+                    <div class='student-testimonial-box'>
+                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+
+                    </div>
+                    <div class='student-testimonial-box'>
+                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                    </div>
+                    <div class='student-testimonial-box'>
+                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                    </div>
+                </div>
+            </div>
         </div >
     );
 }
