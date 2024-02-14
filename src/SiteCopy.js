98c8788e1992a64f1a2ee6299e20ef64663a76fe
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-14 09:27:29 -0800

    Adds a SiteCopy file for section text

---
+++ src/SiteCopy.js
@@ -0,0 +1,35 @@
+const aboutText = () => (
+    <>
+        <p>
+            Nick is a movement teacher based in the Dogpatch neighborhood of San Francisco. He focuses on movement and awareness practices that are fun, caring, kind and gentle.
+        </p>
+        <p>
+            Inspired by Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities and mindfulness, Nick's practice works with the material of the body to learn about creativity, spontaneity, compassion and self love.
+        </p>
+        <p>
+            Contact Nick for a free consultation and first session at <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>
+        </p>
+    </>
+)
+
+const problemText = () => (
+    <>
+        <p>
+            We treat our bodies badly. We ignore them. We keep still and quiet. We eat and move with shame, with an embarrassment we have to drag this dying flesh around behind us as we navigate the world.
+        </p>
+        <p>
+            It's not our fault. It's a byproduct of living in a culture that admires a narrow set of ideals and people. Think about this: nearly all the "exercise", "fitness", "training" you've come across in your life is modeled on one of three fantasies: (1) The Olympian (2) The Soldier (3) The Ballerina.
+        </p>
+        <p>
+            But you don't need to be an Olympian, a soldier or a ballerina. You don't need to be muscular, limber or thin to be healthy. You don't need to want to model yourself on someone else's ideas of beauty, strength or freedom.
+        </p>
+        <p>
+            Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with thoughtless admiration ideals.
+        </p>
+    </>
+)
+
+export const sections = {
+    aboutText: aboutText,
+    problemText: problemText,
+}
