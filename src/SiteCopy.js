17fbd6556ca5c0e10cd91b5b92f1a77299c75b40
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-14 12:26:02 -0800

    Updates Practice copy, updates Teacher copy, updates Fantasies copy

--- src/SiteCopy.js
+++ src/SiteCopy.js
@@ -1,16 +1,20 @@
 const fantasiesText = () => (
     <>
         <p>
-            We treat our bodies badly. We ignore them. We keep still and quiet. We eat and move with shame, with an embarrassment we have to drag this dying flesh around behind us as we navigate the world.
+            We treat our bodies badly. Or we ignore them. We keep still and quiet. We eat and move with shame. We are embarrassed to drag this flesh suit around behind us as we walk through life.
         </p>
         <p>
-            It's not our fault. It's a byproduct of living in a culture that admires a narrow set of ideals and people. Think about this: nearly all the "exercise", "fitness", "training" you've come across in your life is modeled on one of three fantasies: (1) The Olympian (2) The Soldier (3) The Ballerina.
+            It's not our fault. It's a byproduct of living in a world that admires a narrow set of ideals and people. Think about this: nearly all the "exercise", "fitness", "training" you've come across in your life is modeled on one of three fantasies: (1) The Olympian (2) The Soldier (3) The Ballerina.
         </p>
         <p>
             But you don't need to be an Olympian, a soldier or a ballerina. You don't need to be muscular, limber or thin to be healthy. You don't need to want to model yourself on someone else's ideas of beauty, strength or freedom.
         </p>
         <p>
-            Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with thoughtless admiration ideals.
+            Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with arbitrary and unfair ideals.
+        </p>
+
+        <p>
+            Want to start learning about these practices? Get in touch by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
         </p>
     </>
 );
@@ -18,13 +22,19 @@ const fantasiesText = () => (
 const practiceText = () => (
     <>
         <p>
-            Nick is a movement teacher based in the Dogpatch neighborhood of San Francisco. He focuses on movement and awareness practices that are fun, caring, kind and gentle.
+            Discover a practice that expresses non-violent curiosity and caring - for yourself and for the people and things around you.
         </p>
         <p>
-            Inspired by Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities and mindfulness, Nick's practice works with the material of the body to learn about creativity, spontaneity, compassion and self love.
+            Explore a practice that encourages intuitive movement in the moment instead of along pre-determined pathways.
         </p>
         <p>
-            Contact Nick for a free consultation and first session at <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>
+            Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body. <strong>It is possible to forge a personal practice that feels fun and pleasurable forever</strong>.
+        </p>
+        <p>
+            Inspired by Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities, weight sharing games, mindfulness and beautiful movement communities and teachers around the world, this practice works with the material of the body to teach and learn about creativity, spontaneity, compassion and self love. (We also do a lot of just plain goofin' around).
+        </p>
+        <p>
+            Get in touch for a free consultation and introductory movement session by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
         </p>
     </>
 );
@@ -32,7 +42,13 @@ const practiceText = () => (
 const teacherText = () => (
     <>
         <p>
-            Nick is a movement teacher based in the Dogpatch neighborhood of San Francisco. He focuses on movement and awareness practices that are fun, caring, kind and gentle.
+            Nick is a movement teacher based in the Dogpatch neighborhood of San Francisco. He focuses on movement and awareness practices that are fun, caring, kind, gentle and (if you're into this kind of thing) wild.
+        </p>
+        <p>
+            In previous lives Nick has been a founder, sofware engineer, product manager, urban farmer, data-driven pro sports gambler and journalist. What unites these? A curious desire to solve problems in elegant and funny ways.
+        </p>
+        <p>
+            If you're looking to learn about non-violent movement practices intended to serve you for the rest of your life, get in touch for a free consultation and introductory movement session by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
         </p>
     </>
 );
