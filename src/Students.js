66548003654a41703a1058fda7736623ace6664f
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-14 09:37:02 -0800

    Fixes use of class where it should have been className

--- src/Students.js
+++ src/Students.js
@@ -6,22 +6,22 @@ const Students = ({ theme }) => {
             <h1>Students</h1>
             <div className="students-div-wrapper">
                 <div className="students-div-row-1">
-                    <div class='student-testimonial-box'>
+                    <div className='student-testimonial-box'>
                         <text>"Big quote about working with Nick privately here" -- Student</text>
                     </div>
-                    <div class='student-testimonial-box'>
+                    <div className='student-testimonial-box'>
                         <text>"Big quote about working with Nick in community classes here" -- Student</text>
                     </div>
                 </div>
                 <div className="students-div-row-2">
-                    <div class='student-testimonial-box'>
+                    <div className='student-testimonial-box'>
                         <text>"Big quote about working with Nick in community classes here" -- Student</text>
 
                     </div>
-                    <div class='student-testimonial-box'>
+                    <div className='student-testimonial-box'>
                         <text>"Big quote about working with Nick in community classes here" -- Student</text>
                     </div>
-                    <div class='student-testimonial-box'>
+                    <div className='student-testimonial-box'>
                         <text>"Big quote about working with Nick in community classes here" -- Student</text>
                     </div>
                 </div>
