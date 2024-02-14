e3810a7f809eefdbe89d2f72a05180138db07ec7
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-14 09:45:05 -0800

    Removes all references to <text> elements to silence console warnings

--- src/Offerings.js
+++ src/Offerings.js
@@ -3,10 +3,10 @@ const Offerings = ({ theme }) => {
         <div className={`landing-sect ${theme}-theme`}>
             <h1>Let's move together</h1>
             <div className="sexy-cell-container">
-                <div className="sexy-cell"><text>Private Sessions</text></div>
-                <div className="sexy-cell"><text>Community Classes</text></div>
-                <div className="sexy-cell"><text>Programs for Seniors</text></div>
-                <div className="sexy-cell"><text>Ski Instruction</text></div>
+                <div className="sexy-cell">Private Sessions</div>
+                <div className="sexy-cell">Community Classes</div>
+                <div className="sexy-cell">Programs for Seniors</div>
+                <div className="sexy-cell">Ski Instruction</div>
             </div>
         </div>
     );
--- src/Students.js
+++ src/Students.js
@@ -7,22 +7,22 @@ const Students = ({ theme }) => {
             <div className="students-div-wrapper">
                 <div className="students-div-row-1">
                     <div className='student-testimonial-box'>
-                        <text>"Big quote about working with Nick privately here" -- Student</text>
+                        <div>"Big quote about working with Nick privately here" -- Student</div>
                     </div>
                     <div className='student-testimonial-box'>
-                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                        <div>"Big quote about working with Nick in community classes here" -- Student</div>
                     </div>
                 </div>
                 <div className="students-div-row-2">
                     <div className='student-testimonial-box'>
-                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                        <div>"Big quote about working with Nick in community classes here" -- Student</div>
 
                     </div>
                     <div className='student-testimonial-box'>
-                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                        <div>"Big quote about working with Nick in community classes here" -- Student</div>
                     </div>
                     <div className='student-testimonial-box'>
-                        <text>"Big quote about working with Nick in community classes here" -- Student</text>
+                        <div>"Big quote about working with Nick in community classes here" -- Student</div>
                     </div>
                 </div>
             </div>
