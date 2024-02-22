bb4cd2fb77076346470864f26785563dc902372f
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-22 07:29:30 -0800

    Updates site copy

--- src/SiteCopy.js
+++ src/SiteCopy.js
@@ -13,25 +13,28 @@ const fantasiesText = () => (
             Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with arbitrary and unfair ideals.
         </p>
 
-        <p>
+        {/* <p>
             Want to start learning about these practices? Get in touch by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
-        </p>
+        </p> */}
     </>
 );
 
 const practiceText = () => (
     <>
         <p>
-            Discover a practice that expresses curiosity and caring - for yourself and for the people and things around you.
+            Explore a practice that expresses curiosity and caring - for yourself and for the people and things around you.
         </p>
         <p>
-            Explore a practice that encourages intuitive movement in the moment instead of along pre-determined pathways.
+            Discover a practice that encourages intuitive movement in the moment instead of along pre-determined pathways.
         </p>
         <p>
-            Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body. <strong>It is possible to forge a personal practice that feels fun and pleasurable forever</strong>.
+            Work with the material of the body to teach and learn about creativity, spontaneity, compassion and self love. We also do a lot of plain goofin' around.
         </p>
         <p>
-            Inspired by Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities, weight sharing games, mindfulness and beautiful movement communities and teachers around the world, this practice works with the material of the body to teach and learn about creativity, spontaneity, compassion and self love. (We also do a lot of just plain goofin' around).
+            Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body. <strong>It is possible to learn to move in a way that feels fun and pleasurable forever</strong>.
+        </p>
+        <p>
+            The practice draws inspiration from many disciplines including: Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities, weight sharing games, mindfulness and many more beautiful movement communities and teachers around the world. But you don't need to worry about any of these if you don't want to.
         </p>
         <p>
             Get in touch for a free consultation and introductory movement session by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
