91fe0287807c14d2baa93a772cb5fb7e2de6dafa
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-22 07:55:06 -0800

    Updates Practice copy, cleans up site copy

--- src/SiteCopy.js
+++ src/SiteCopy.js
@@ -12,47 +12,48 @@ const fantasiesText = () => (
         <p>
             Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with arbitrary and unfair ideals.
         </p>
-
-        {/* <p>
+        <p>
             Want to start learning about these practices? Get in touch by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
-        </p> */}
+        </p>
     </>
 );
 
 const practiceText = () => (
     <>
         <p>
-            Explore a practice that expresses curiosity and caring - for yourself and for the people and things around you.
+            Work with the material of the body.
         </p>
         <p>
-            Discover a practice that encourages intuitive movement in the moment instead of along pre-determined pathways.
+            Seek curiosity and caring, spontaneity, creativity and compassion through movement. Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body.
         </p>
         <p>
-            Work with the material of the body to teach and learn about creativity, spontaneity, compassion and self love. We also do a lot of plain goofin' around.
+            <strong>It is possible to learn to move in a way that feels fun and pleasurable forever</strong>.
         </p>
         <p>
-            Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body. <strong>It is possible to learn to move in a way that feels fun and pleasurable forever</strong>.
+            Want to learn how? Email <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a> for a free introduction.
         </p>
         <p>
-            The practice draws inspiration from many disciplines including: Tai-Chi, Qigong, Yoga, Calisthenics, Somatics, The Feldenkrais Method, The Ido Portal Method, improvisational dance modalities, weight sharing games, mindfulness and many more beautiful movement communities and teachers around the world. But you don't need to worry about any of these if you don't want to.
+            Although you don't need to know about any of these, this teaching draws inspiration from many traditions including:
+            calisthenics,
+            improvisational dance modalities,
+            mindfulness,
+            qigong,
+            somatics,
+            tai-chi,
+            the Feldenkrais Method,
+            the Ido Portal Method,
+            weight sharing games,
+            yoga,
+            and many more beautiful movement communities and teachers around the world.
         </p>
         <p>
-            Get in touch for a free consultation and introductory movement session by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
+            Love to move. Love yourself. Love your body. Love the people and things around you.
         </p>
     </>
 );
 
 const teacherText = () => (
     <>
-        {/* <p>
-            Nick is a movement teacher based in the Dogpatch neighborhood of San Francisco. He focuses on movement and awareness practices that are fun, caring, kind, gentle and (if you're into this kind of thing) wild.
-        </p>
-        <p>
-            In previous lives Nick has been a founder, sofware engineer, product manager, urban farmer, data-driven pro sports gambler and journalist. What unites these? A curious desire to solve problems in elegant and funny ways.
-        </p>
-        <p>
-            If you're looking to learn about non-violent movement practices intended to serve you for the rest of your life, get in touch for a free consultation and introductory movement session by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
-        </p> */}
         <p>
             Hi, I'm Nick.
         </p>
