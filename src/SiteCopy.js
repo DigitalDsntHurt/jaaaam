3e3f93b33b5475e1a301f39de863e091501725b3
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-02-22 08:15:08 -0800

    Refactors site copy to use objects with titles and bodies - moves away from functions

--- src/SiteCopy.js
+++ src/SiteCopy.js
@@ -1,91 +1,105 @@
-const fantasiesText = () => (
-    <>
-        <p>
-            We treat our bodies badly. Or we ignore them. We keep still and quiet. We eat and move with shame. We are embarrassed to drag this flesh suit around behind us as we walk through life.
-        </p>
-        <p>
-            It's not our fault. It's a byproduct of living in a world that admires a narrow set of ideals and people. Think about this: nearly all the "exercise", "fitness", "training" you've come across in your life is modeled on one of three fantasies: (1) The Olympian (2) The Soldier (3) The Ballerina.
-        </p>
-        <p>
-            But you don't need to be an Olympian, a soldier or a ballerina. You don't need to be muscular, limber or thin to be healthy. You don't need to model yourself on someone else's ideas of beauty, strength or freedom.
-        </p>
-        <p>
-            Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with arbitrary and unfair ideals.
-        </p>
-        <p>
-            Want to start learning about these practices? Get in touch by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
-        </p>
-    </>
-);
+const fantasiesText = {
+    title: 'Fantasies',
+    body: (
+        <>
+            <p>
+                We treat our bodies badly. Or we ignore them. We keep still and quiet. We eat and move with shame. We are embarrassed to drag this flesh suit around behind us as we walk through life.
+            </p>
+            <p>
+                It's not our fault. It's a byproduct of living in a world that admires a narrow set of ideals and people. Think about this: nearly all the "exercise", "fitness", "training" you've come across in your life is modeled on one of three fantasies: (1) The Olympian (2) The Soldier (3) The Ballerina.
+            </p>
+            <p>
+                But you don't need to be an Olympian, a soldier or a ballerina. You don't need to be muscular, limber or thin to be healthy. You don't need to model yourself on someone else's ideas of beauty, strength or freedom.
+            </p>
+            <p>
+                Physical practices exist that will keep you safe, healthy and whole on your own terms, without beating you over the head with arbitrary and unfair ideals.
+            </p>
+            <p>
+                Want to start learning about these practices? Get in touch by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
+            </p>
+        </>
+    ),
+};
 
-const practiceText = () => (
-    <>
-        <p>
-            Work with the material of the body.
-        </p>
-        <p>
-            Seek curiosity and caring, spontaneity, creativity and compassion through movement. Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body.
-        </p>
-        <p>
-            <strong>It is possible to learn to move in a way that feels fun and pleasurable forever</strong>.
-        </p>
-        <p>
-            Want to learn how? Email <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a> for a free introduction.
-        </p>
-        <p>
-            Although you don't need to know about any of these, this teaching draws inspiration from many traditions including:
-            calisthenics,
-            improvisational dance modalities,
-            mindfulness,
-            qigong,
-            somatics,
-            tai-chi,
-            the Feldenkrais Method,
-            the Ido Portal Method,
-            weight sharing games,
-            yoga,
-            and many more beautiful movement communities and teachers around the world.
-        </p>
-        <p>
-            Love to move. Love yourself. Love your body. Love the people and things around you.
-        </p>
-    </>
-);
+const practiceText = {
+    title: 'Movement Classes for Everyone',
+    body: (
+        <>
+            <p>
+                Work with the material of the body.
+            </p>
+            <p>
+                Seek curiosity and caring, spontaneity, creativity and compassion through movement. Most of all, seek fun and pleasure. Not stories about fun and pleasure but the real thing. Felt by you in your body.
+            </p>
+            <p>
+                <strong>It is possible to learn to move in a way that feels fun and pleasurable forever</strong>.
+            </p>
+            <p>
+                Want to learn how? Email <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a> for a free introduction.
+            </p>
+            <p>
+                Although you don't need to know about any of these, this teaching draws inspiration from many traditions including:
+                calisthenics,
+                improvisational dance modalities,
+                mindfulness,
+                qigong,
+                somatics,
+                tai-chi,
+                the Feldenkrais Method,
+                the Ido Portal Method,
+                weight sharing games,
+                yoga,
+                and many more beautiful movement communities and teachers around the world.
+            </p>
+            <p>
+                Love to move. Love yourself. Love your body. Love the people and things around you.
+            </p>
+        </>
+    )
+};
 
-const teacherText = () => (
-    <>
-        <p>
-            Hi, I'm Nick.
-        </p>
-        <p>
-            I'm a movement teacher in the Dogpatch neighborhood of San Francisco. I focus on movement and awareness practices that are fun, caring, kind, gentle and (if you're into this kind of thing) wild.
-        </p>
-        <p>
-            I was born and grew up in San Francisco (Upper Haight) and currently I'm obsessed with SF- and neighborhood-level community and community movement practices. In previous lives I've been a founder, sofware engineer, product manager, urban farmer, data-driven sports gambler and journalist. What unites these? A curious desire to solve problems in elegant and funny ways.
-        </p>
-        <p>
-            If you're looking to learn about non-violent movement practices intended to serve you for the rest of your life, get in touch for a free intro by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
-        </p>
-    </>
-);
+const welcomeText = {
+    title: 'This is for you.',
+    body: (
+        <>
+            <p>
+                Move your body.
+            </p>
+            <p>
+                Bring awareness to your body.
+            </p>
+            <p>
+                Discover the delights that follow.
+            </p>
+        </>
+    ),
+};
 
-const welcomeText = () => (
-    <>
-        <p>
-            Move your body.
-        </p>
-        <p>
-            Bring awareness to your body.
-        </p>
-        <p>
-            Discover the delights that follow.
-        </p>
-    </>
-)
+const teacherText = {
+    title: 'Teacher',
+    body: (
+        <>
+            <p>
+                Hi, I'm Nick.
+            </p>
+            <p>
+                I'm a movement teacher in the Dogpatch neighborhood of San Francisco. I focus on movement and awareness practices that are fun, caring, kind, gentle and (if you're into this kind of thing) wild.
+            </p>
+            <p>
+                I was born and grew up in San Francisco (Upper Haight) and currently I'm obsessed with SF- and neighborhood-level community and community movement practices. In previous lives I've been a founder, sofware engineer, product manager, urban farmer, data-driven sports gambler and journalist. What unites these? A curious desire to solve problems in elegant and funny ways.
+            </p>
+            <p>
+                If you're looking to learn about non-violent movement practices intended to serve you for the rest of your life, get in touch for a free intro by emailing <a href='mailto:grapesdance@gmail.com'>grapesdance@gmail.com</a>.
+            </p>
+        </>
+    ),
+};
 
-export const sectionText = {
-    practice: practiceText,
+const sectionText = {
     fantasies: fantasiesText,
+    practice: practiceText,
     teacher: teacherText,
     welcome: welcomeText,
-}
+};
+
+export default sectionText;
