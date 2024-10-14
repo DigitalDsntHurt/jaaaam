f122c09fee307e63cba350e43170a0cc668ba15a
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-10-14 12:21:16 -0700

    Adds a new route for CommunityClasses page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Events from './pages/Events';
 import EventsLanding from './pages/EventsLanding';
+import CommunityClasses from './pages/CommunityClasses';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<EventsLanding />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/community-classes" element={<CommunityClasses />} />
       </Routes>
     </BrowserRouter>
   );
