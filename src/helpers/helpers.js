896b764b5f8f9345cf0d59969ddf3c682e5532af
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-10-14 13:49:29 -0700

    Writes new helpers to get a list of upcoming dates

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -8,6 +8,7 @@ function formatDate(date) {
     return date.toLocaleDateString('en-US', options);
 }
 
+// this funciton returns a list of dates where the first date is today's date and the last date is January 1, 2025
 function generateDateList() {
     const startDate = new Date(); // Today's date
     const endDate = new Date('2025-01-01'); // January 1, 2025
@@ -20,6 +21,27 @@ function generateDateList() {
     return dateList;
 }
 
+const getNUpcomingDates = (n, dayOfWeek) => {
+    const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+    const dates = [];
+    let currentDate = new Date();
+
+    // Find the next relevant day of week
+    while (currentDate.getDay() !== daysOfWeek.indexOf(dayOfWeek.toLowerCase())) {
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    // Add the next n days of week
+    for (let i = 0; i < n; i++) {
+        dates.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 7); // Move to next week
+    }
+
+    return dates.map(date => formatDate(date));
+}
+
 export {
     generateDateList,
+    getNUpcomingDates,
 }
