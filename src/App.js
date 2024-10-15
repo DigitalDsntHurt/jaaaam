bc9b1e45b049c7713084d4af1da76bd756da115a
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-10-15 16:14:24 -0700

    Adds route for testimonials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BannerBioSchedule from './pages/BannerBioSchedule';
 import CommunityClasses from './pages/CommunityClasses';
 import EventsLanding from './pages/archive/EventsLanding';
+import Testimonials from './pages/Testimonials';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<BannerBioSchedule />} />
         <Route path="/blah-blah" element={<EventsLanding />} />
         <Route path="/community-classes" element={<CommunityClasses />} />
+        <Route path="/testimonials" element={<Testimonials />} />
       </Routes>
     </BrowserRouter>
   );
