bc829e43d97cb0667ed499f77d729328f09d8cdb
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2024-10-15 17:18:02 -0700

    Adds home header link to testimonials page

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -4,6 +4,9 @@ import Students from "../content_components/Students";
 const Testimonials = () => {
     return (
         <>
+            <div className='custom-hero-floating-header-link'>
+                <a href='/'>Home</a>
+            </div>
             <Students />
             <Footer theme='light' />
         </>
