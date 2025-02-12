64c9f6ed9ca84012abb5e02aeafca32ab713f89d
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2025-02-12 11:06:55 -0800

    Cleans up hero styling

--- src/content_components/HomeHero.js
+++ src/content_components/HomeHero.js
@@ -1,27 +1,30 @@
 import './HomeHero.scss';
 
 const HomeHero = () => {
+
+    const images = [
+        '/images/half_moon.png',
+        '/images/teaching.jpg',
+        '/images/back_to_back.jpg',
+        '/images/head_down.jpg',
+        '/images/holly_park.png',
+        '/images/bridge_at_chase.png',
+    ];
+
     return (
-        <div className='custom-hero-container' >
+        <div className='custom-hero-container'>
             <div className='custom-hero-floating-header-link'>
                 <a href='/testimonials'>Testimonials</a>
             </div>
-            <img
-                alt='holistic movement with nick lala smith'
-                className="custom-hero-image"
-                src='./images/half_moon.png'
-            />
-        </div >
+            <div className='custom-hero-image-container'>
+                <img
+                    alt='holistic movement with nick lala smith'
+                    className="custom-hero-image"
+                    src={images[0]}
+                />
+            </div>
+        </div>
     );
 }
 
 export default HomeHero;
-
-// const images = [
-//     '/images/half_moon.png',
-//     '/images/teaching.jpg',
-//     '/images/back_to_back.jpg',
-//     '/images/head_down.jpg',
-//     '/images/holly_park.png',
-//     '/images/bridge_at_chase.png',
-// ];
