e310756e924e184af861d3c211b2a1580ed55532
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2025-08-16 11:35:35 -0700

    Adds optional custom props to Bio component

--- src/content_components/Bio.js
+++ src/content_components/Bio.js
@@ -1,14 +1,16 @@
 import { bioText } from '../site_copy/SiteCopy';
 import ImageLeftTextRight from "../generic_components/ImageLeftTextRight"
 
-const Bio = ({ theme }) => {
+const Bio = ({ image, title, theme }) => {
+    const imagePath = image || './images/flattered.png';
+    const titleText = title || 'About Nick'
     return (
         <>
             <ImageLeftTextRight
-                imagePath='./images/flattered.png'
+                imagePath={imagePath}
                 text={bioText}
                 theme={theme}
-                title="About Nick"
+                title={titleText}
             />
         </>
     )
