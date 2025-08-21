2c21719e393ca90e0727373d78a1397b30229d53
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2025-08-20 23:51:12 -0700

    adds newsletter signup link to upcoming events header

--- src/generic_components/events/Events.js
+++ src/generic_components/events/Events.js
@@ -6,7 +6,11 @@ import regularEventItemsList from "../../site_copy/regularEvents";
 const Events = () => {
     return (
         <>
-            <TitleBanner bannerText='UPCOMING' />
+            <TitleBanner 
+                bannerText='UPCOMING' 
+                cornerNav={true}
+                subText={(<p style={{textAlign: 'center'}}><a href='https://docs.google.com/forms/d/e/1FAIpQLScT49DHIYllF6Ftk7o5_ox203fLKdItqb_Z-8EiyhAVAG6I4A/viewform' target='_blank' rel='noreferrer'>Newsletter Sign-up</a></p>)}
+            />
             <EventsList events={upcomingEventItemsList} />
             <div id='weekly-classes'>
                 <TitleBanner bannerText='REGULAR SCHEDULE' />
