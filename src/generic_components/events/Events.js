a1b710758c2c264744f0bb2022dc9affe6d71068
Nicholas Grapes-Badu <nicholasgrapes-badu@Nicholass-MBP.home>
2025-08-20 23:59:29 -0700

    Fixes homepage bug

--- src/generic_components/events/Events.js
+++ src/generic_components/events/Events.js
@@ -8,7 +8,7 @@ const Events = () => {
         <>
             <TitleBanner 
                 bannerText='UPCOMING' 
-                cornerNav={true}
+                // cornerNav={true}
                 subText={(<p style={{textAlign: 'center'}}><a href='https://docs.google.com/forms/d/e/1FAIpQLScT49DHIYllF6Ftk7o5_ox203fLKdItqb_Z-8EiyhAVAG6I4A/viewform' target='_blank' rel='noreferrer'>Newsletter Sign-up</a></p>)}
             />
             <EventsList events={upcomingEventItemsList} />
